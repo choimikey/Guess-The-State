e0d8e7dcf308fa29ccfff4648558e0f022623c52
Michael Choi <132119138+choimikey@users.noreply.github.com>
2023-07-25 20:23:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 import turtle
 import pandas
 
-# TODO: 1. Convert the guess to Title case
-# TODO: 2. Check if the guess is among the 50 states
-# TODO: 3. Write a correct guesses onto the map
-# TODO: 4. Use a loop to allow the user to keep guessing
-# TODO: 5. Record the correct guesses in a list
-# TODO: 6. Keep track of the score
-
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
@@ -20,7 +13,10 @@ guessed_states = []
 is_game_over = True
 
 
+# Uses a loop to allow the user to keep guessing
+
 while len(guessed_states) < 50:
+    # Keeping track of the score & converts the guess to "Title Case"
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's name?").title()
     if answer_state == "Exit":
@@ -31,6 +27,7 @@ while len(guessed_states) < 50:
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
+        # Check if the guess is among the 50 states
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
@@ -40,11 +37,6 @@ while len(guessed_states) < 50:
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
 
-
-    # If answer_state is one of the states in all the states of the 50_states.csv
-         #If they got it right:
-            #Create a turtle to write the name of the state at the state's X and Y coordinates
-#Save the missing states to states_to_learn.csv (create a new file)
 
 
 
